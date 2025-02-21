4f1cb9ac4cd106518a129e112e92eceffa860d17
ouyangshiliang <ouyangshiliang223@gmail.com>
2025-02-21 10:40:50 +0800

    Update mac2mqtt.go

--- mac2mqtt.go
+++ mac2mqtt.go
@@ -117,29 +117,7 @@ func getCurrentVolume() int {
 }
 
 
-func getCurrentLock() bool {
-	// 使用 `ioreg` 命令获取 Root 信息
-	ioregCmd := exec.Command("ioreg", "-n", "Root", "-d1", "-a")
-	var ioregOut bytes.Buffer
-	ioregCmd.Stdout = &ioregOut
-	if err := ioregCmd.Run(); err != nil {
-		fmt.Println("Error running ioreg:", err)
-		return
-	}
-	// 使用 `PlistBuddy` 命令解析 plist 并获取屏幕锁定状态
-	plistBuddyCmd := exec.Command("/usr/libexec/PlistBuddy", "-c", "print :IOConsoleUsers:0:CGSSessionScreenIsLocked", "stdin")
-	plistBuddyCmd.Stdin = &ioregOut
-	var plistBuddyOut bytes.Buffer
-	plistBuddyCmd.Stdout = &plistBuddyOut
-	if err := plistBuddyCmd.Run(); err != nil {
-		fmt.Println("Error running PlistBuddy:", err)
-		return
-	}	
-	l, err := strconv.ParseBool(plistBuddyCmd.Stdout)
-	if err != nil {
-	}
-	return l
-}
+
 
 
 
